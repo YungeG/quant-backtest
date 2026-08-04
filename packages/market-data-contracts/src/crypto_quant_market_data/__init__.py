15555539d6be77532d6958184750e5718585d4e1
YungeG <yungeguo@outlook.com>
2026-08-04 21:41:02 +0800

    [WP-06A] implement market bundle reader contracts

--- packages/market-data-contracts/src/crypto_quant_market_data/__init__.py
+++ packages/market-data-contracts/src/crypto_quant_market_data/__init__.py
@@ -1,3 +1,37 @@
 """Immutable market-data bundle contracts."""
 
+from .bundles import (
+    EventCursor,
+    InMemoryMarketBundleReader,
+    InputValidationFailure,
+    InputValidationIssue,
+    InputValidationIssueCode,
+    MarketBundleCapability,
+    MarketBundleError,
+    MarketBundleIntegrityError,
+    MarketBundleManifest,
+    MarketBundleReader,
+    MarketBundleRef,
+    MarketBundleStreamError,
+    MarketEvent,
+    MarketStreamManifest,
+)
+
 __version__ = "0.1.0"
+
+__all__ = [
+    "EventCursor",
+    "InMemoryMarketBundleReader",
+    "InputValidationFailure",
+    "InputValidationIssue",
+    "InputValidationIssueCode",
+    "MarketBundleCapability",
+    "MarketBundleError",
+    "MarketBundleIntegrityError",
+    "MarketBundleManifest",
+    "MarketBundleReader",
+    "MarketBundleRef",
+    "MarketBundleStreamError",
+    "MarketEvent",
+    "MarketStreamManifest",
+]
